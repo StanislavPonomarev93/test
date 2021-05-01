30dd4a67375c80018cbe995fa4b6d78d2b1623bd
Stanislav Ponomarev <chemburleno@yandex.ru>
2021-05-01 09:20:57 +0300

    first commit

--- js/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('title').innerHTML = `Width: ${document.getElementById('container').offsetWidth}, Height: ${document.getElementById('container').offsetHeight}`;
-document.getElementById('input').addEventListener('focus', function () {
-  document.getElementById('title').innerHTML = `Width: ${document.getElementById('container').offsetWidth}, Height: ${document.getElementById('container').offsetHeight}`;
-});
